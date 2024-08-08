b56b76abb51d6e6b2d66eac8a3498ee2837caf31
Charles Ferguson <gerph@gerph.org>
2024-08-08 01:06:53 +0100

    Add strncat implementation.
    Another small function that we need.

--- clib/string.c
+++ clib/string.c
@@ -35,6 +35,23 @@ char *strcat(char *a, const char *b)
 }
 
 
+char *strncat(char *a, const char *b, size_t n)
+{
+    char *oa = a;
+    /* Find the end of the string */
+    while (*a++)
+        ;
+    while (--n >= 0)
+    {
+        char c = *b++;
+        *a++ = c;
+        if (c=='\0')
+            break;
+    }
+    return oa;
+}
+
+
 int strncmp(const char *a, const char *b, size_t n)
 {
     while (n-->0)
