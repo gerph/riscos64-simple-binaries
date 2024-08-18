260040a4982d71872dfd93a669b1428d5413c682
Charles Ferguson <gerph@gerph.org>
2024-08-18 10:31:08 +0100

    Fix for _kernel_oscli implementation.
    The _kernel_oscli implementation was documented according to the PRM
    which meant that it was listed as returning an indication of an error.
    The actual implementation is to return 1 if we are successful.

--- clib/kernel-cli.c
+++ clib/kernel-cli.c
@@ -6,6 +6,15 @@
 
 extern _kernel_oserror *_last_error;
 
+/*************************************************** Gerph *********
+ Function:      _kernel_oscli
+ Description:   Run a CLI command and report whether it errored - this
+                may replace the current application.
+ Parameters:    cmd-> the command to run
+ Returns:       1 if ok, 0 if failed
+ NOTE:          This is implemented according to the actual implementation.
+                The PRM implies that the return indicates that there is an error.
+ ******************************************************************/
 int _kernel_oscli(const char *cmd)
 {
     _kernel_swi_regs regs = {0};
@@ -14,5 +23,5 @@ int _kernel_oscli(const char *cmd)
     err = _kernel_swi(OS_CLI, &regs, NULL);
     if (err)
         _last_error = err;
-    return err != NULL;
+    return err == NULL;
 }
