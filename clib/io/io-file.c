0d579a5e4c2a66bd42f89d9231ae3cc95c6708c9
Charles Ferguson <gerph@gerph.org>
2025-05-05 10:30:03 +0100

    Separate off fgets as it's larger now that it uses OS_ReadLine.
    fgets is a larger function now, and it might not be used that often,
    so we move it out to is own file so that it doesn't get linked to
    other things.

--- clib/io/io-file.c
+++ clib/io/io-file.c
@@ -3,15 +3,7 @@
 #include <string.h>
 #include "swis.h"
 #include "swis_os.h"
-
-
-#define _IO_MAGIC (0x381F0000)
-#define _IO_MAGIC_MASK (0xFFFF0000)
-#define CHECK_MAGIC(fh, fail_code) \
-                        do { \
-                            if ( ((fh)->_flags & _IO_MAGIC_MASK) != _IO_MAGIC ) \
-                                return (fail_code); \
-                        } while (0)
+#include "io/io-internal.h"
 
 extern FILE *__file_list;
 
@@ -285,81 +277,6 @@ int fputc(int c, FILE *fh)
 int putc(int c, FILE *fh)
 {
     return fputc(c, fh);
-}
-
-/*************************************************** Gerph *********
- Function:      os_readline
- Description:   Call OS_ReadLine for input
- Parameters:    line-> line to read
-                len = max length including terminator
- Returns:       1 for success, 0 for failure
- ******************************************************************/
-static int os_readline(char *line, int len)
-{
-    _kernel_oserror *err;
-    int read;
-    uint32_t flags = 0;
-#ifdef __riscos64
-    err = _swix(OS_ReadLine32, _INR(0, 4)|_OUTR(0, 1), line, len, 32, 128, 0, &flags, &read);
-    if (err)
-        return 0;
-    if (flags)
-        goto escape;
-    line[read] = '\0';
-    return 1;
-#else
-    err = _swix(OS_ReadLine32, _INR(0, 4)|_OUT(1)|_OUT(_FLAGS), line, len, 32, 128, 0, &read, &flags);
-#ifndef ErrorNumber_ModuleBadSWI
-#define ErrorNumber_ModuleBadSWI          0x110 /*  Token for internationalised message */
-#endif
-    if (err && err->errnum == ErrorNumber_ModuleBadSWI)
-    {
-        err = _swix(OS_ReadLine, _INR(0, 4)|_OUT(1)|_OUT(_FLAGS), line, len, 32, 128, 0, &read, &flags);
-    }
-    if (err)
-        return 0;
-    if (flags & _C)
-        goto escape;
-    line[read] = '\0';
-    return 1;
-#endif
-
-escape:
-    return 0;
-}
-
-char *fgets(char *str, int size, FILE *fh)
-{
-    char *p;
-    if (!fh)
-        return NULL;
-
-    if (fh == stdin)
-    {
-        /* They want to read a line from stdin. Might as well use OS_ReadLine */
-        if (!os_readline(str, size))
-            return NULL;
-        return str;
-    }
-    if (fh == stdout || fh == stderr)
-    {
-        return NULL;
-    }
-
-    CHECK_MAGIC(fh, NULL);
-
-    for (p = str; p - str < size - 1; p++)
-    {
-        int c = fgetc(fh);
-        if (c == -1)
-            return NULL;
-        if (c != '\n')
-            *p = c;
-        else
-            break;
-    }
-    *p = '\0';
-    return p;
 }
 
 int fputs(const char *str, FILE *fh)
