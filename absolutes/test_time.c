ea112eff24bd87c0932519bba31f818e805d9d98
Charles Ferguson <gerph@gerph.org>
2025-07-12 19:12:03 +0100

    Improve our testing with better reporting about the test runs.
    We now have test runs that execute each test individually on the
    build service, and check the results. They're then reported into
    a JUnit XML file which is then reported on. This hopefully means
    that we should get some much more readable results from our
    test jobs.

--- absolutes/test_time.c
+++ absolutes/test_time.c
@@ -2,13 +2,20 @@
 #include <time.h>
 #include "swis.h"
 
+/* Maximum seconds to wait */
+#define LIMIT (5)
+
 int main(void)
 {
     int i;
+    time_t start = time(NULL);
     for (i=0; i<300; i++)
     {
+        time_t now = time(NULL);
         printf("time = %i\n", time(NULL));
         _swix(OS_Byte, _IN(0), 19);
+        if (now - start > LIMIT)
+            break;
     }
     return 0;
 }
